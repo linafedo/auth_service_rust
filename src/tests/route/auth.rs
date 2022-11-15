ebab4dc514865f7b459bed6f9a011bc4fefad664
linafedo <gv.fedo@gmail.com>
2022-11-15 21:28:26 +0300

    add authentication tests

--- src/tests/route/auth.rs
+++ src/tests/route/auth.rs
@@ -45,3 +45,47 @@ async fn registration_returns_a_400_when_data_is_missing() {
         response.status().as_u16()
     );
 }
+
+#[tokio::test]
+async fn authentication_returns_a_200_for_valid_form_data() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = serde_json::json!({
+        "login": "username",
+        "password": "password"
+    });
+    println!("{}", body);
+    let response = client
+        .get(&format!("{}/authentication", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .form(&body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn authentication_returns_a_400_when_data_is_missing() {
+    let address = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let body = serde_json::json!({
+        "login": "username",
+    });
+
+    let response = client
+        .get(&format!("{}/authentication", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .form(&body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(
+        400,
+        response.status().as_u16()
+    );
+}
